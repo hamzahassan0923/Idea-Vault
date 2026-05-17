0743dd2574f12b1d76dc064d0365ce1f74c431f7
hamzahassan0923 <hamzahassan0923@gmail.com>
2026-05-18 01:06:58 +0600

    added navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="navbar sticky top-0 z-50 bg-white/20 backdrop-blur border-b border-base-300 mx-auto container">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      </div>
+      <ul
+        tabIndex="-1"
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li><Link href={'/'} >Home</Link></li>
+        <li><Link href={"/ideas"}>Ideas</Link></li>
+        <li><Link href={"/add-idea"}>Add Idea</Link></li>
+        <li><Link href={"/my-ideas"}>My Ideas</Link></li>
+        <li><Link href={"/my-interactions"}>My Interactions</Link></li>
+       
+      </ul>
+    </div>
+    <Link href={"/"} className="font-bold text-xl">IdeaVault</Link>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+     <li><Link href={'/'} className="hover:text-blue-300 ">Home</Link></li>
+        <li><Link href={"/ideas"} className="hover:text-blue-300 ">Ideas</Link></li>
+        <li><Link href={"/add-idea"} className="hover:text-blue-300 ">Add Idea</Link></li>
+        <li><Link href={"/my-ideas"} className="hover:text-blue-300 ">My Ideas</Link></li>
+        <li><Link href={"/my-interactions"} className="hover:text-blue-300 ">My Interactions</Link></li>
+    </ul>
+  </div>
+  <div className="navbar-end">
+    <ul className='flex gap-4'>
+         <li><Link href={"/login"}><button className="hover:text-blue-300 ">Login</button></Link></li>
+         <li><Link href={"/register"}><button className='hover:text-blue-300'>Register</button></Link></li>
+      
+    </ul>
+  </div>
+</div>
+    );
+};
+
+export default Navbar;
